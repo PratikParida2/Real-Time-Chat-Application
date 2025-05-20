d3825fda4630909bfef59587696f488536264f67
Pratik Parida <paridapratik709@gmail.com>
2025-05-20 19:55:10 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,11 @@ app.use(cors({
 }));
 app.use(cookieParser());
 dotenv.config();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
   });
 }
 app.get('/',(req,res)=>{
