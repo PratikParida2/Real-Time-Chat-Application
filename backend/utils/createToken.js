bed5aa593c50112a98ea626fd037ede29440dbc4
Pratik Parida <paridapratik709@gmail.com>
2025-05-20 11:24:32 +0530

    solving some issues

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -9,7 +9,7 @@ const createToken=(res,id)=>
 
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: "strict",
         maxAge: 30 * 24 * 60 * 60 * 1000,
       });
